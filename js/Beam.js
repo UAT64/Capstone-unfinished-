22307ad299731a0e0894573cfcfd9d69fd1368b0
UAT64 <90963605+UAT64@users.noreply.github.com>
2022-03-12 14:14:51 +0000

    Add files via upload

---
+++ js/Beam.js
@@ -0,0 +1,25 @@
+class Beam {
+    constructor(x, y, w, h) {
+      let options = {
+        isStatic: true
+      };
+  
+      this.body = Bodies.rectangle(x, y, w, h, options);
+      this.image = loadImage("./assets/beam.png")
+      this.w = w;
+      this.h = h;
+      this.color = color;
+      World.add(world, this.body);
+    }
+  
+    show() {
+      let pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill("black");
+      rect(0, 0, this.w, this.h);
+      
+      pop();
+    }
+  }
